9b75e2240152d4dcdaed5cf6e04d1f6079a751b5
Dave Laird <kiwiupover@gmail.com>
2019-03-18 22:45:34 -0700

    WIP

--- addon/components/x-button/component.js
+++ addon/components/x-button/component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import layout from './template';
+import layout from 'css-module-addon/components/x-button/template';
 
 export default Component.extend({
   layout
