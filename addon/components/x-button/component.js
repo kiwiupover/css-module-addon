bf132a492dd1fcd05e4bcd362cf6293009684948
Dave Laird <kiwiupover@gmail.com>
2019-03-19 07:59:20 -0700

    WIP

--- addon/components/x-button/component.js
+++ addon/components/x-button/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
-import layout from 'css-module-addon/components/x-button/template';
+import layout from './template';
 
 export default Component.extend({
-  layout
+  layout,
+  tagName: ''
 });
